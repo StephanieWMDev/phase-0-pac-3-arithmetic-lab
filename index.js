efbb0278847ad3fbecc069adac336c84c5507811
Stephanie Milano <orbitaboudoir@gmail.com>
2022-03-07 21:03:36 +1100

    completed assignement

---
+++ index.js
@@ -0,0 +1,18 @@
+// number =10;
+
+// function increment(n) {
+//     return number += 1;
+// }
+
+// increment();
+
+// function decrement(n) {
+//     return number -= 1;
+// }
+
+// increment();
+// decrement();
+//---------------------------------------------
+// 
+
+
